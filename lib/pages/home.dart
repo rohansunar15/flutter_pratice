6c87a3b6498baeb1392ca67cc85edd92974fe26e
rohan <rohansunar12@gmail.com>
2023-02-04 13:27:40 +0530

    3rd edit

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,7 +14,123 @@ class _HomeState extends State<Home>{
     appBar: AppBar(
       title: Text('APPLICATION 2'),
     ),
-    body: Container(),
+    body: SafeArea(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(18.0),
+          child:  Column(
+            children: <Widget>[
+              Rowcolumn()
+            ],
+          ),
+        ),
+      ),
+    ),
+    );
+  }
+}
+
+class Rowcolumn extends StatelessWidget {
+  const Rowcolumn({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Container(
+              color: Colors.red,
+              height: 50.0,
+              width: 50.0,
+
+            ),
+            Padding(padding: EdgeInsets.all(15.0)),
+            Container(
+              color: Colors.lightGreen,
+              height: 50.0,
+              width: 50.0,
+            ),
+            Padding(padding: EdgeInsets.all(15.0)),
+             Container(
+              color: Colors.lightBlueAccent,
+              height: 50.0,
+              width: 50.0,
+            ),
+            Divider(),
+            Row(
+              children: <Widget>[
+                circleavatar(),
+
+              ],
+            ),
+            Divider(),
+            Text('The End ')
+          ],
+        ),
+        Container(
+          color: Colors.brown,
+          height: 50.0,
+          width: 50.0,
+        ),
+        Padding(padding: EdgeInsets.all(15.0)),
+        Expanded(child: Container(
+          color: Colors.yellow,
+          height: 50.0,
+          width: 50.0,
+        ),
+        ),
+        Padding(padding: EdgeInsets.all(15.0)),
+        Expanded(child: Container(
+          color: Colors.lightBlue,
+          height: 50.0,
+          width: 50.0,
+        ),
+        ),
+      ],
+    );
+  }
+}
+
+class circleavatar extends StatelessWidget {
+  const circleavatar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      backgroundColor: Colors.purpleAccent,
+      radius: 60.0,
+
+      child: Stack(
+        children: <Widget>[
+          Container(
+            height: 80.0,
+            width: 80.0,
+            color: Colors.grey.shade700
+          ),
+          Container(
+            height: 60.0,
+            width: 60.0,
+            color: Colors.amberAccent,
+          ),
+          Container(
+            color: Colors.greenAccent.shade700,
+            height: 40.0,
+            width: 40.0,
+          ),
+          Container(
+            color: Colors.blueAccent.shade700,
+            height: 20.0,
+            width: 20.0,
+          ),
+        ],
+      ),
     );
   }
 }
